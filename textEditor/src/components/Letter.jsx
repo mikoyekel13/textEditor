aa36e681d222aeef3c339fcca11dd89e63c842cd
mikoyekel13 <mikoyekel@hilma.tech>
2023-11-12 11:03:13 +0200

    letter button

--- textEditor/src/components/Letter.jsx
+++ textEditor/src/components/Letter.jsx
@@ -1,3 +1,15 @@
-function Letter(props) {}
+import React, { useState } from "react";
+
+function Letter(props) {
+  return (
+    <button
+      className="letterButton"
+      id={`${props.char}-Char`}
+      onClick={props.typeChar}
+    >
+      {props.char}
+    </button>
+  );
+}
 
 export default Letter;
