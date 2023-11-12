46e82a61171e751a5a93a6521b06d2dca52c30bf
mikoyekel13 <mikoyekel@hilma.tech>
2023-11-12 16:41:57 +0200

    s

--- textEditor/src/components/StyleButtons.jsx
+++ textEditor/src/components/StyleButtons.jsx
@@ -1,50 +1,49 @@
 import KeyBoard from "./KeyBoard";
 function StyleButton(props) {
-    function changeTextColor() {
-        props.textStyle(function (prev) {
-            const newStyle = { ...prev };
-            { newStyle.color === 'red' ? newStyle.color = 'black' : newStyle.color = 'red' }
-            return newStyle;
-        })
-    }
-    function changeTextFont() {
-        props.textStyle(function (prev) {
-            const newStyle = { ...prev };
-            const Fonts = ['Arial', 'Labster', 'Quicksand','Didot','Roboto','Garamond'];
-            let random = Math.floor(Math.random() * Fonts.length);
-            console.log(random);
-            newStyle.fontFamily = Fonts[random];
-            // newStyle.fontFamily = newStyle.fontFamily === 'Arial' ? 'Cantarell' : 'Arial';
-            return newStyle;
-        })
-    }
-
-  function changeToUpperCase() {
-    props.textStyle((prev) => {
-      const newStyle = { ...prev };
-
-      // Assuming there is a property called 'text' in the style object
-      if (newStyle.text) {
-        newStyle.text = newStyle.text.toUpperCase();
-      }
-
-      return newStyle;
-    });
-  }
-  function changeToFifteen() {
+  function changeTextColor(e) {
     props.textStyle(function (prev) {
       const newStyle = { ...prev };
-      newStyle.fontSize = newStyle.fontSize === "20px" ? "30px" : "20px";
+      newStyle.color = `${e.target.value}`;
       return newStyle;
     });
   }
-   
+  function changeTextFont() {
+    props.textStyle(function (prev) {
+      const newStyle = { ...prev };
+      const Fonts = [
+        "Arial",
+        "Labster",
+        "Quicksand",
+        "Didot",
+        "Roboto",
+        "Garamond",
+      ];
+      let random = Math.floor(Math.random() * Fonts.length);
+      console.log(random);
+      newStyle.fontFamily = Fonts[random];
+      // newStyle.fontFamily = newStyle.fontFamily === 'Arial' ? 'Cantarell' : 'Arial';
+      return newStyle;
+    });
+  }
+
+  function changeFontSize(e) {
+    props.textStyle(function (prev) {
+      const newStyle = { ...prev };
+      newStyle.fontSize = `${e.target.value}px`;
+      return newStyle;
+    });
+  }
+
   return (
     <div>
-      <button onClick={changeTextColor}>Red</button>
+      <input onChange={changeTextColor} type="color"></input>
       <button onClick={changeTextFont}>change Font</button>
-      <button onClick={changeToFifteen}>30px</button>
-        
+      <input
+        type="number"
+        onChange={changeFontSize}
+        placeholder="font size"
+      ></input>
+
       <button
         id="changeAllTextStyle"
         onClick={() =>
