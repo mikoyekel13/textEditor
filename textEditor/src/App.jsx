f6fecc117568eb5aa2166c2f8f41fa25744b284a
mikoyekel13 <mikoyekel@hilma.tech>
2023-11-12 11:28:42 +0200

    keyboard

--- textEditor/src/App.jsx
+++ textEditor/src/App.jsx
@@ -1,18 +1,13 @@
 import React, { useState } from "react";
 import "./App.css";
-<<<<<<< HEAD
 import KeyBoard from "./components/KeyBoard";
-=======
-import KeyBoard from "./components/KeyBoard"
-function App() {
->>>>>>> c02b7b613a0b51d4cb85d3e6f1b7bba54d8124e0
 
 function App() {
-  const [text, setText] = useState("trial");
+  const [text, setText] = useState("");
   return (
     <>
       <div id="showText">{text}</div>
-      <KeyBoard />
+      <KeyBoard text={setText} />
     </>
   );
 }
--- textEditor/src/components/KeyBoard.jsx
+++ textEditor/src/components/KeyBoard.jsx
@@ -1,56 +1,15 @@
-<<<<<<< HEAD
 import Letter from "./Letter";
-
-function KeyBoard() {
-  const letters = [
-    a,
-    b,
-    c,
-    d,
-    e,
-    f,
-    g,
-    h,
-    i,
-    j,
-    k,
-    l,
-    m,
-    n,
-    o,
-    p,
-    q,
-    r,
-    s,
-    t,
-    u,
-    v,
-    w,
-    x,
-    y,
-    z,
-  ];
+function KeyBoard(props) {
+  const letters = "a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z";
+  function typeChar(char) {
+    props.text((prev) => `${prev}${char}`);
+  }
   return (
     <div>
-      {letters.map((letter, index) => (
-        <Letter char={letter} />
+      {letters.split(",").map((letter, index) => (
+        <Letter char={letter} key={index} onClick={() => typeChar(letter)} />
       ))}
     </div>
   );
 }
-
 export default KeyBoard;
-=======
-import Letter from "./Letter"
-function KeyBoard(){
-    const letters='abcdefghijklmnopqrstuvwxyz';
-    return <div>
-        {letters.map((letter,index)=>
-        (
-            <Letter char={letter}/>
-        )
-        )}
-    </div>
-}
-export default KeyBoard;
->>>>>>> c02b7b613a0b51d4cb85d3e6f1b7bba54d8124e0
