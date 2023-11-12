a7def2d40cd0058cd61544b825660172e2135a14
Shirshlomo18 <Shirshlomo@hilma.tech>
2023-11-12 11:29:15 +0200

    second

--- textEditor/src/components/KeyBoard.jsx
+++ textEditor/src/components/KeyBoard.jsx
@@ -1,51 +1,8 @@
-<<<<<<< HEAD
-import Letter from "./Letter";
-
-function KeyBoard() {
-  const letters = [
-    a,
-    b,
-    c,
-    d,
-    e,
-    f,
-    g,
-    h,
-    i,
-    j,
-    k,
-    l,
-    m,
-    n,
-    o,
-    p,
-    q,
-    r,
-    s,
-    t,
-    u,
-    v,
-    w,
-    x,
-    y,
-    z,
-  ];
-  return (
-    <div>
-      {letters.map((letter, index) => (
-        <Letter char={letter} />
-      ))}
-    </div>
-  );
-}
-
-export default KeyBoard;
-=======
 import Letter from "./Letter"
 function KeyBoard(){
-    const letters='abcdefghijklmnopqrstuvwxyz';
+    const letters='a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z';
     return <div>
-        {letters.map((letter,index)=>
+        {letters.split(',').map((letter,index)=>
         (
             <Letter char={letter}/>
         )
@@ -53,4 +10,3 @@ function KeyBoard(){
     </div>
 }
 export default KeyBoard;
->>>>>>> c02b7b613a0b51d4cb85d3e6f1b7bba54d8124e0
